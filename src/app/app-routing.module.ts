fd557d15794e26c08983fd9dcd307932a6413bac
ClementRoelens <clement.roelens@hotmail.com>
2023-02-02 23:37:11 +0100

    Lancement sur le module film par défaut

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'film',loadChildren:()=>import('./feature/film/film.routing-module').then(m=>m.FilmRoutingModule)},
+  // {path:'',component:HomeComponent},
+  // Pour le moment seuls les films sont disponbiles, on aura plus tard du lazy loading pour les deux autres modèles
+  {path:'',loadChildren:()=>import('./feature/film/film.routing-module').then(m=>m.FilmRoutingModule)},
+  // {path:'film',loadChildren:()=>import('./feature/film/film.routing-module').then(m=>m.FilmRoutingModule)},
   {path:'**',component:HomeComponent}
 ];
 
