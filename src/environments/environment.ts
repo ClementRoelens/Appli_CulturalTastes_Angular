81df7635b2951a37ad533d21b2eaa1a35a322ce3
Clement <clement.roelens@hotmail.com>
2023-07-20 10:43:23 +0200

    Corrections CSS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // Dev
-  apiUrl:"https://localhost:7068",
+  apiUrl:"http://localhost:5000",
   // Une fois déployé
   // apiUrl:"https://culturaltastesapi.azurewebsites.net",
   // imageStorageUrl:"https://culturaltastesstockage.blob.core.windows.net"
